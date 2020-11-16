01db78d011f54e4fc4ab6b4b1b51fdd989abd478
drchibs <richarddennis789@gmail.com>
2020-11-16 17:03:01 +0100

    Basic Notifications for simple Tasks now implemented

--- background.js
+++ background.js
@@ -2,8 +2,26 @@
 
 const API_URL = "http://127.0.0.1:3000/api/v1/chips";
 
-//const sendChip = async (url) => {
-async function sendChip(url) {
+/**
+ *
+ * @param {creates a chrome notification with the message provided it!} message
+ */
+const createChipNotification = (message, contextMessage = null) => {
+    chrome.notifications.create(
+        "chip-it",
+        {
+            type: "basic",
+            iconUrl: "chip-128.png",
+            title: "CHIP IT!",
+            message: message,
+            contextMessage: contextMessage,
+        },
+        function (notificationId) {}
+    );
+};
+
+const sendChip = async (url) => {
+    //async function sendChip(url) {
     //This function sends a chip to the server and returns a status and data object
     try {
         //before we create a new chip we need to verify that this chip doesn't already exist for this users
@@ -26,7 +44,7 @@ async function sendChip(url) {
         //console.log(err);
         return err.message;
     }
-}
+};
 
 const createChipAction = () => {
     //get the active tab url
@@ -35,12 +53,19 @@ const createChipAction = () => {
         //send the URL (chip) our server
         try {
             const data = sendChip(url).then((res) => {
-                //console.log(res);
-                alert(res.status);
                 //send a notification to the browser based on status of the response = SUCCESS or FAIL
+                let note = "";
+                if (res.status == "success") {
+                    note = `You chipped this Page!`;
+                    subNote = `see your new chip at chrome://bookmarks`;
+                } else {
+                    note = `You failed to chip this Page!`;
+                }
+                createChipNotification(note, subNote);
             });
         } catch (err) {
             //send a notification to the browser based on error in request = ERROR
+            createChipNotification(err.message);
         }
     });
 };
@@ -60,6 +85,9 @@ chrome.runtime.onInstalled.addListener(function () {
         contexts: ["page", "link"],
     });
     //throw a welcome notification
+    const note = `Hi, You're now using Chip It!`;
+    const subNote = `Chip It! was built by D.R Chibs: https://github.com/drchibs , Twitter: https://twitter.com/el_chibs`;
+    createChipNotification(note, subNote);
 });
 
 //listen for a click event on the browser page
@@ -82,13 +110,17 @@ const printBookmarks = (bookmarks) => {
             //at this point we make an api call to save them for this user
             try {
                 const data = sendChip(bookmark.url).then((res) => {
-                    //console.log(res);
                     console.log(res.status);
                     //send a notification to the browser based on status of the response = SUCCESS or FAIL
                 });
             } catch (err) {
                 //send a notification to the browser based on error in request = ERROR
+                createChipNotification(err.message);
+                return;
             }
+            //we don't want to send a notification for every bookmark saved, imagine a user has 80, so we're sending just one notification for everything and we're not even sure that saving was successful.
+            note = `All your bookmarks were saved successfully!`;
+            createChipNotification(note);
             //console.log(bookmark.url);
         }
     });
